d213241488adc1ce70d702f7d24454fbd8efe114
ddariod <dariod.dazaa09@gmail.com>
2024-11-21 17:18:26 -0500

    form validation success

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'frontend-challenge';
+  userForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+    email: new FormControl('', [Validators.required, Validators.email])
+  })
+
+  onSubmit() {
+    if (this.userForm.valid) {
+      console.log('sdusahdas');
+      console.log(this.userForm.value);
+      this.userForm.reset();
+    }
+  }
 }
